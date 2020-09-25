f2152b34926f82693b70162d7864ac2d1cf8c7ff
MaxBrohman <maksimbrochman@gmail.com>
2020-09-25 13:55:02 +0300

    routing added

--- src/Main.elm
+++ src/Main.elm
@@ -1,27 +1,43 @@
 module Main exposing (main, view)
 
 import Browser exposing (Document)
+import Browser.Navigation as Nav
 import Html exposing (Html, a, caption, footer, h1, li, nav, text, ul)
 import Html.Attributes exposing (classList, href)
+import Html.Lazy exposing (lazy)
+import Url exposing (Url)
+import Url.Parser as Parser exposing ((</>), Parser, s, string)
 
 
 type alias Model =
-    { page : Page }
+    { page : Page, key : Nav.Key }
 
 
 type Page
-    = Gallery
+    = SelectedImage String
+    | Gallery
     | Folders
     | NotFound
 
 
 type Msg
-    = NothingYet
+    = ClickedLink Browser.UrlRequest
+    | ChangedUrl Url
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        ClickedLink urlRequest ->
+            case urlRequest of
+                Browser.External href ->
+                    ( model, Nav.load href )
+
+                Browser.Internal url ->
+                    ( model, Nav.pushUrl model.key (Url.toString url) )
+
+        ChangedUrl url ->
+            ( { model | page = urlToPage url }, Cmd.none )
 
 
 subscriptions : Model -> Sub Msg
@@ -37,7 +53,7 @@ view model =
     in
     { title = "Photo Groove"
     , body =
-        [ viewHeader model.page
+        [ lazy viewHeader model.page
         , content
         , viewFooter
         ]
@@ -63,16 +79,63 @@ viewHeader page =
 
         navLink : Page -> { url : String, caption : String } -> Html msg
         navLink targetPage { url, caption } =
-            li [ classList [ ( "active", page == targetPage ) ] ]
+            li [ classList [ ( "active", isActive { link = targetPage, page = page } ) ] ]
                 [ a [ href url ] [ text caption ] ]
     in
     nav [] [ logo, links ]
 
 
+isActive : { link : Page, page : Page } -> Bool
+isActive { link, page } =
+    case ( link, page ) of
+        ( Gallery, Gallery ) ->
+            True
+
+        ( Gallery, _ ) ->
+            False
+
+        ( Folders, Folders ) ->
+            True
+
+        ( Folders, SelectedImage _ ) ->
+            True
+
+        ( Folders, _ ) ->
+            False
+
+        ( SelectedImage _, _ ) ->
+            False
+
+        ( NotFound, _ ) ->
+            False
+
+
+init : () -> Url -> Nav.Key -> ( Model, Cmd Msg )
+init flags url key =
+    ( { page = urlToPage url, key = key }, Cmd.none )
+
+
+urlToPage : Url -> Page
+urlToPage url =
+    Parser.parse parser url
+        |> Maybe.withDefault NotFound
+
+
+parser : Parser (Page -> a) a
+parser =
+    Parser.oneOf
+        [ Parser.map Folders Parser.top
+        , Parser.map Gallery (s "gallery")
+        , Parser.map SelectedImage (s "images" </> Parser.string)
+        ]
+
+
 main : Program () Model Msg
 main =
-    Browser.document
-        { init = \_ -> ( { page = Folders }, Cmd.none )
+    Browser.application
+        { init = init
+        , onUrlRequest = ClickedLink
+        , onUrlChange = ChangedUrl
         , subscriptions = subscriptions
         , update = update
         , view = view
